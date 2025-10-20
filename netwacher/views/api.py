03458a33dea6e4aed3304fe39ab914a8d7afa925
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-20 22:14:24 +0700

    feat: api route

--- netwacher/views/api.py
+++ netwacher/views/api.py
@@ -3,3 +3,24 @@ from netwacher.controllers import *
 
 
 api = Blueprint('api', __name__)
+
+@api.route("/active-interface")
+def active_interface():
+    return jsonify({
+        "status": 1,
+        "data": get_active_interface()
+    })
+    
+@api.route("/ip-addr")
+def ip_addr():
+    return jsonify({
+        "status": 1,
+        "data": get_ip_addr
+    })
+
+@api.route("/scan-ip/<module_name>")
+def scan_ip(module_name):
+    return jsonify({
+        "status": 1,
+        "data": scan_ip(module_name)
+    })
