d91d1f2f16b3b2e671fd8e5e45dcd26546e63aaf
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-21 11:03:15 +0700

    fix: func name for route

--- netwacher/views/api.py
+++ netwacher/views/api.py
@@ -6,20 +6,21 @@ api = Blueprint('api', __name__)
 
 @api.route("/active-interface")
 def active_interface():
+    data = get_active_interface_ctr()
     return jsonify({
         "status": 1,
-        "data": get_active_interface()
+        "data": data
     })
     
 @api.route("/ip-addr")
 def ip_addr():
     return jsonify({
         "status": 1,
-        "data": get_ip_addr
+        "data": get_ip_addr()
     })
 
 @api.route("/scan-ip/<module_name>")
-def scan_ip(module_name):
+def scan_ip_api(module_name):
     return jsonify({
         "status": 1,
         "data": scan_ip(module_name)
