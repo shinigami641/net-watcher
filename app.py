30c67a0149fa505e233268c0493317da2d2d1ce9
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-21 11:01:54 +0700

    fix: call api var

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from netwacher.exstension import db, socketio
-from netwacher.views import api
+from netwacher.views.api import api
 from config import Config
 
 def create_app(config_object=Config):
@@ -14,7 +14,9 @@ def create_app(config_object=Config):
     # register blueprint (route/view)
     app.register_blueprint(api, url_prefix='/api')
     
+    return app
+    
 if __name__ == '__main__':
     app = create_app()
     # gunakan socketio.run agar WebSocket aktif
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='127.0.0.1', port=4000)
