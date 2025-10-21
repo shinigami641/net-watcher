4c9d7159993699812b0562ab155ad92c788696f4
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-21 21:52:45 +0700

    feat: controller use payload

--- netwacher/controllers/client_controller.py
+++ netwacher/controllers/client_controller.py
@@ -2,11 +2,23 @@ from netwacher.models.client import Info
 
 info = Info()
 
+ALLOWED_PAYLOAD_KEYS = {
+    "module_name"
+}
+
 def get_active_interface_ctr():
     return info.get_active_interface()
 
 def get_ip_addr():
     return info.get_ip_addr()
 
-def scan_ip(module_name):
-    return info.scan_ip(module_name)
+def scan_ip(payload: dict):
+    if not isinstance(payload, dict):
+        return None
+    
+    filtered = {k: payload[k] for k in payload.keys() & ALLOWED_PAYLOAD_KEYS }
+    
+    try:
+        return info.scan_ip(**filtered)
+    except Exception as e:
+        return None
