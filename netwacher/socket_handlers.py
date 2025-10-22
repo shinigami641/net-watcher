7ecc278454d2c11486d888c04c8bab5a13f6f698
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-22 20:02:03 +0700

    feat: add join room for spesific room not broadcast

--- netwacher/socket_handlers.py
+++ netwacher/socket_handlers.py
@@ -16,7 +16,13 @@ class NotificationsNamespace(Namespace):
     def on_client_message(self, data):
         print('received from client:', data)
         emit('server_response', {'ok': True})
+        
+    def on_join_room(self, data):
+        room = data.get("room")
+        if room:
+            join_room(room)
+            emit("joined", {"room": room})
 
-def send_ws_tes(event_name, data, namespace='/notifications', room=None):
+def send_ws_tes(event_name, data=dict, namespace='/notifications', room=None):
     from netwacher.exstension import socketio
     socketio.emit(event_name, data, namespace=namespace, room=room)
