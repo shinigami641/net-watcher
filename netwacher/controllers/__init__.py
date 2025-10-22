04f0a6ec018923298ebc0f908359ab273469c0f0
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-22 20:02:48 +0700

    feat: add stop scan job in __all__

--- netwacher/controllers/__init__.py
+++ netwacher/controllers/__init__.py
@@ -1,4 +1,4 @@
 from .client_controller import get_ip_addr, get_active_interface_ctr, scan_ip
-from .traffic_controller import traffict_scan_ip
+from .traffic_controller import traffict_scan_ip, stop_scan_job
 
-__all__ = ["get_ip_addr", "scan_ip", "get_active_interface_ctr", "traffict_scan_ip"]
+__all__ = ["get_ip_addr", "scan_ip", "get_active_interface_ctr", "traffict_scan_ip", "stop_scan_job"]
