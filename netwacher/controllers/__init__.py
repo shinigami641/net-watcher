c4154375ac90d7cc2dbcadc77ae94e8639f5eb4f
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-29 21:46:28 +0700

    feat: add func for use in api route

--- netwacher/controllers/__init__.py
+++ netwacher/controllers/__init__.py
@@ -1,4 +1,4 @@
-from .client_controller import get_ip_addr, get_active_interface_ctr, scan_ip, get_ip_gateway
+from .client_controller import get_ip_addr, get_active_interface_ctr, scan_ip, get_ip_gateway, get_info_detail_client
 from .traffic_controller import traffict_scan_ip, stop_scan_job, ACTIVE_SCANS
 
-__all__ = ["get_ip_addr", "scan_ip", "get_active_interface_ctr", "traffict_scan_ip", "stop_scan_job", "ACTIVE_SCANS", "get_ip_gateway"]
+__all__ = ["get_ip_addr", "scan_ip", "get_active_interface_ctr", "traffict_scan_ip", "stop_scan_job", "ACTIVE_SCANS", "get_ip_gateway", "get_info_detail_client"]
