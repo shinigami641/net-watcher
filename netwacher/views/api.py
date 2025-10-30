ea07457483f45d90d9f1530ed2bedf7423bd3c57
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-30 17:52:30 +0700

    feat: add return in api gateway

--- netwacher/views/api.py
+++ netwacher/views/api.py
@@ -25,6 +25,7 @@ def ip_gateway():
     data = get_ip_gateway()
     if data is None:
         return error_response("Ip Gateway not found", http_status=404, app_code=APP_ERROR_CODES["IP_GATEWAY_NOT_FOUND"])
+    return success_response(data=data, message="Ip Gateway Found", http_status=200)
 
 @api.route("/info-detail-client/<ip>")
 def info_detail_client(ip):
