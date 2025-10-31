b7e1a6189afeb466482b9e0d636f8e1cdf29bdb3
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-10-31 23:04:18 +0700

    fix: fixing config

--- web/src/config/api.js
+++ web/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000/api';
+export const BASE_URL = 'http://localhost:4000/api';
 
 export const api = axios.create({
   baseURL: BASE_URL,
